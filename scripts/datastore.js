3f7d0b563d5294565595573e715fb49343d2076d
Alex Hansen <7299702@isd535.org>
2022-03-14 11:08:46 -0500

    getting the datastore prototype

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -10,7 +10,15 @@
     DataStore.prototype.add = function (key, val) {
         this.data[key] = val;
     };
-    
+
+    DataStore.prototype.get = function (key) {
+        return this.data[key];
+    };
+
+    DataStore.prototype.getAll = function () {
+        return this.data;
+    }
+
     App.DataStore = DataStore;
     window.App = App;
 })(window);
