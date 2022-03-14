6dc20be42faf3b44187ea476edcdc46489f13bd1
Alex Hansen <7299702@isd535.org>
2022-03-14 11:10:19 -0500

    DataStore.prototype.remove

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -17,6 +17,10 @@
 
     DataStore.prototype.getAll = function () {
         return this.data;
+    };
+
+    DataStore.prototype.remove = function(key) {
+        delete this.data[key];
     }
 
     App.DataStore = DataStore;
