83a64a8e08c5b3272d06e84bd94f094b5269aa99
Alex Hansen <7299702@isd535.org>
2022-03-14 11:04:06 -0500

    DataStore prototype

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -3,9 +3,14 @@
     let App = window.App || {};
 
     function DataStore() {
-        console.log('Running the DataStore function...')
+        console.log('Running the DataStore function...');
+        this.data = {};
     }
 
+    DataStore.prototype.add = function (key, val) {
+        this.data[key] = val;
+    };
+    
     App.DataStore = DataStore;
     window.App = App;
 })(window);
