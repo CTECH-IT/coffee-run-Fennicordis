797fef06f2d1d5948a52ffaa3e5e9e7bf6d1e445
Alex Hansen <7299702@isd535.org>
2022-03-14 10:49:16 -0500

    DataStore function

---
+++ scripts/datastore.js
@@ -0,0 +1,11 @@
+(function (window) {
+    'use strict';
+    let App = window.App || {};
+
+    function DataStore() {
+        console.log('Running the DataStore function...')
+    }
+
+    App.DataStore = DataStore;
+    window.App = App;
+})(window);
