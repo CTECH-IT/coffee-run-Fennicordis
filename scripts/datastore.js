afbcf93861025e32431811878e7cf404e5cf70bb
Alex Hansen <7299702@isd535.org>
2022-03-15 11:08:14 -0500

    foundation of coffee run

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -2,26 +2,27 @@
     'use strict';
     let App = window.App || {};
 
-    function DataStore() {
-        console.log('Running the DataStore function...');
-        this.data = {};
+    class DataStore {
+        constructor() {
+            this.data = {};
+        }
+        add(key, val) {
+            this.data[key] = val;
+        }
+        get(key) {
+            return this.data[key];
+        }
+        getAll() {
+            return this.data;
+        }
+        remove(key) {
+            delete this.data[key];
+        }
     }
 
-    DataStore.prototype.add = function (key, val) {
-        this.data[key] = val;
-    };
 
-    DataStore.prototype.get = function (key) {
-        return this.data[key];
-    };
 
-    DataStore.prototype.getAll = function () {
-        return this.data;
-    };
 
-    DataStore.prototype.remove = function (key) {
-        delete this.data[key];
-    }
 
     App.DataStore = DataStore;
     window.App = App;
---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+(function (window) {
+    'use strict';
+    let App = window.App;
+    let Truck = App.Truck;
+    let DataStore = App.DataStore;
+
+    let myTruck = new Truck('12345', new DataStore());
+
+    window.myTruck = myTruck;
+
+})(window);
