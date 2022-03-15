4835aa26cdbc9ecb714e526d7e58b9121f73acba
Alex Hansen <7299702@isd535.org>
2022-03-15 11:11:04 -0500

    final check in of Unit 4 Part 1

--- scripts/truck.js
+++ scripts/truck.js
@@ -2,32 +2,34 @@
     'use strict';
     let App = window.App || {};
 
-    function Truck(truckId, db) {
-        this.truckId = truckId;
-        this.db = db;
+    class Truck {
+        constructor(truckId, db) {
+            this.truckId = truckId;
+            this.db = db;
+        }
+        createOrder(order) {
+            console.log('Adding order for ' + order.emailAddress);
+            this.db.add(order.emailAddress, order);
+        }
+        deliverOrder(customerId) {
+            console.log('Delivering order for ' + customerId);
+            this.db.remove(customerId);
+        }
+        printOrders() {
+
+            // first, get all the email addresses (keys)
+            let customerIdArray = Object.keys(this.db.getAll());
+
+            console.log('Truck #' + this.truckId + ' has pending orders:');
+            // go through the list of emails and get the associated order
+            customerIdArray.forEach(function (id) {
+                console.log(this.db.get(id));
+            }.bind(this));
+        }
     }
 
-    Truck.prototype.createOrder = function (order) {
-        console.log('Adding order for ' + order.emailAddress);
-        this.db.add(order.emailAddress, order);
-    }
 
-    Truck.prototype.deliverOrder = function (customerId) {
-        console.log('Delivering order for ' + customerId);
-        this.db.remove(customerId);
-    }
 
-    Truck.prototype.printOrders = function () {
-
-        // first, get all the email addresses (keys)
-        let customerIdArray = Object.keys(this.db.getAll());
-
-        console.log('Truck #' + this.truckId + ' has pending orders:');
-        // go through the list of emails and get the associated order
-        customerIdArray.forEach(function (id) {
-            console.log(this.db.get(id));
-        }.bind(this));
-    }
 
     App.Truck = Truck;
     window.App = App;
