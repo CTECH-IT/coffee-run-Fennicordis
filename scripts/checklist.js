ae8c5ea3f82b174c1456b785259da8bde52588a5
Alex Hansen <7299702@isd535.org>
2022-03-18 11:20:39 -0500

    adding rows(can't remove rows when there 2 orders)

--- scripts/checklist.js
+++ scripts/checklist.js
@@ -14,24 +14,49 @@
             }
         }
     }
-    function Row(coffeeOrder) {
-        let $div = $('<div></div>', {
-            'data-coffee-order': 'checkbox',
-            'class': 'checkbox'
-        });
-        let $label = $('<label></label>');
-        let $checkbox = $('input></input>', {
-            type: 'checkbox',
-            value: coffeeOrder.emailAddress
-        });
 
-        let description = coffeeOrder.size + ' ';
-        if (coffeeOrder.flavor) {
-            description += coffeeOrder.flavor + ', ';
+    Checklist.prototype.addRow = function (coffeeOrder) {
+        // Remove any existing rows that match the email address
+        this.removeRow(coffeeOrder.emailAddress);
+        // Create a new instance of a row, using the coffe order info
+        var rowElement = new Row(coffeeOrder);
+        // Add the new row instance's $element property to the checklist
+        this.$element.append(rowElement.$element);
+    };
+
+    Checklist.prototype.removeRow = function (email) {
+        this.$element
+            .find('[value="' + email + '"]')
+            .closest('[data-coffee-order="checklist"]')
+            .remove();
+    };
+
+    class Row {
+        constructor(coffeeOrder) {
+            let $div = $('<div></div>', {
+                'data-coffee-order': 'checkbox',
+                'class': 'checkbox'
+            });
+            let $label = $('<label></label>');
+            let $checkbox = $('input></input>', {
+                type: 'checkbox',
+                value: coffeeOrder.emailAddress
+            });
+
+            let description = coffeeOrder.size + ' ';
+            if (coffeeOrder.flavor) {
+                description += coffeeOrder.flavor + ', ';
+            }
+            description += coffeeOrder.coffee + ', ';
+            description += ' (' + coffeeOrder.emailAddress + ')';
+            description += ' [' + coffeeOrder.strength + 'x]';
+
+            $label.append($checkbox);
+            $label.append(description);
+            $div.append($label);
+
+            this.$element = $div;
         }
-        description += coffeeOrder.coffee + ', ';
-        description += ' (' + coffeeOrder.emailAddress + ')';
-        description += ' [' + coffeeOrder.strength + 'x]';
     }
 
     // Add the checklist to the App namespace
--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,22 @@
 (function (window) {
     'use strict';
     const FORM_SELECTOR = '[data-coffee-order="form"]';
+    const CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
     let App = window.App;
     let Truck = App.Truck;
     let DataStore = App.DataStore;
     let FormHandler = App.FormHandler;
+    let Checklist = App.Checklist
 
     let myTruck = new Truck('12345', new DataStore());
+    let checkList = new Checklist(CHECKLIST_SELECTOR);
+    let formHandler = new FormHandler(FORM_SELECTOR);
 
     window.myTruck = myTruck;
 
-    let formHandler = new FormHandler(FORM_SELECTOR);
-    formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
-    console.log(formHandler);
+    formHandler.addSubmitHandler(function (data) {
+        myTruck.createOrder.call(myTruck, data);
+        checkList.addRow.call(checkList, data);
+    });
 
 })(window);
